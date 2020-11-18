d854a5f1b1375aab36b39518da285c2878d10074
daniel <beltrefrancodaniel@gmail.com>
2020-11-17 23:28:57 -0400

    listado Delete

--- Data/IntegranteService.cs
+++ Data/IntegranteService.cs
@@ -1,9 +1,11 @@
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ChurchApp.Data;
+using BlazorInputFile;
+using System.IO;
 
 
 
@@ -17,9 +19,13 @@ namespace ChurchApp.Data
      
      private AplicacionDbContext DbContext;
 
-     public  IntegranteService(AplicacionDbContext dbContext)
+     private readonly IWebHostEnvironment webHostEnvironment;  
+
+     public  IntegranteService(AplicacionDbContext dbContext,IWebHostEnvironment Environment)
      {
          this.DbContext = dbContext;
+
+         this.webHostEnvironment = Environment;
 
      }
      public async Task <List<Modelo>> GetModelosAsync()
@@ -27,20 +33,29 @@ namespace ChurchApp.Data
          return await DbContext.Integrantes.ToListAsync();
 
      }
-     public async Task <Modelo> AddIntegranteAsync(Modelo modelo)
+     public async Task <Modelo> AddIntegranteAsync(Modelo modelo, IFileListEntry file)
      {
-         try
-         {
-             DbContext.Integrantes.Add(modelo);
-             await DbContext.SaveChangesAsync();
+        try
+        {
+            var path = Path.Combine(webHostEnvironment.ContentRootPath, "./wwwroot/Images", file.Name);
+            var streamwiter = new MemoryStream();
+            await file.Data.CopyToAsync(streamwiter);
+            FileStream files = new FileStream(path, FileMode.Create, FileAccess.Write);
+            {
+                streamwiter.WriteTo(files);
+                    modelo.Foto = path.Substring(path.LastIndexOf("Images"));
+            }
+            DbContext.Integrantes.Add(modelo);
+                await DbContext.SaveChangesAsync();
+        }
+        catch(Exception)
+        {
+              
+           throw;
 
-         }
-         catch(Exception)
-         {
-             throw;
+        }
 
-         }
-         return modelo;
+        return modelo;
 
      }
      public async Task<Modelo> UpdateIntegranteAsync(Modelo modelo)
